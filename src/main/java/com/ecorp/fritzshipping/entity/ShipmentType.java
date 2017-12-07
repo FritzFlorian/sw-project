ee3409e855938461f6215e8b01eb1db92623b211
FritzFlorian <flo.fritz@t-online.de>
2017-12-07 12:09:10 +0100

    Add enum types needed in other entities

---
+++ src/main/java/com/ecorp/fritzshipping/entity/ShipmentType.java
@@ -0,0 +1,12 @@
+package com.ecorp.fritzshipping.entity;
+
+/**
+ * Different types of shipments have different restrictions,
+ * costs etc.
+ */
+public enum ShipmentType {
+    LETTER,
+    SMALL_PARCEL,
+    PARCEL,
+    PACKAGE
+}
---
+++ src/main/java/com/ecorp/fritzshipping/entity/TrackingType.java
@@ -0,0 +1,12 @@
+package com.ecorp.fritzshipping.entity;
+
+/**
+ * Describes different types of stops that an package
+ * can be handed over during it's delivery.
+ */
+public enum TrackingType {
+    PICKUP,
+    HAND_IN,
+    PACKAGE_CENTER,
+    DELIVERY
+}

72b3e394109504540ca460d59d37a015901662e6
FritzFlorian <flo.fritz@t-online.de>
2017-12-07 12:29:41 +0100

    Add TrackingNotification entity

---
+++ src/main/java/com/ecorp/fritzshipping/entity/TrackingNotification.java
@@ -0,0 +1,37 @@
+package com.ecorp.fritzshipping.entity;
+
+import com.ecorp.fritzshipping.entity.util.GeneratedLongIdEntity;
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+public class TrackingNotification extends GeneratedLongIdEntity{
+    private String email;
+    private boolean onlyLastPoint;
+    
+    public TrackingNotification(){ 
+        // Empty Default Constructor
+    }
+
+    public TrackingNotification(String email, boolean onlyLastPoint) {
+        this.email = email;
+        this.onlyLastPoint = onlyLastPoint;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isOnlyLastPoint() {
+        return onlyLastPoint;
+    }
+
+    public void setOnlyLastPoint(boolean onlyLastPoint) {
+        this.onlyLastPoint = onlyLastPoint;
+    }
+}

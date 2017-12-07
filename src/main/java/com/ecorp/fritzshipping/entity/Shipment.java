170eecdc242dd464162427b20b401f380f50cb72
FritzFlorian <flo.fritz@t-online.de>
2017-12-07 13:00:14 +0100

    Add addresses to shipment

--- src/main/java/com/ecorp/fritzshipping/entity/Shipment.java
+++ src/main/java/com/ecorp/fritzshipping/entity/Shipment.java
@@ -2,6 +2,10 @@ package com.ecorp.fritzshipping.entity;
 
 import com.ecorp.fritzshipping.entity.util.RandomUUIDEntity;
 import java.util.Collection;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -12,6 +16,25 @@ public class Shipment extends RandomUUIDEntity{
     private int weight;
     private boolean pickup;
     
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name="addressee",column=@Column(name="senderAddressee")),
+        @AttributeOverride(name="street",column=@Column(name="senderStreet")),
+        @AttributeOverride(name="houseNumber",column=@Column(name="senderHouseNumber")),
+        @AttributeOverride(name="city",column=@Column(name="senderCity")),
+        @AttributeOverride(name="postalCode",column=@Column(name="senderPostalCode")),
+    })
+    private Address sender;
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name="addressee",column=@Column(name="recipientAddressee")),
+        @AttributeOverride(name="street",column=@Column(name="recipientStreet")),
+        @AttributeOverride(name="houseNumber",column=@Column(name="recipientHouseNumber")),
+        @AttributeOverride(name="city",column=@Column(name="recipientCity")),
+        @AttributeOverride(name="postalCode",column=@Column(name="recipientPostalCode")),
+    })
+    private Address recipient;
+    
     @OneToMany
     private Collection<TrackingPoint> trackingPoints;
     @OneToMany
@@ -21,9 +44,11 @@ public class Shipment extends RandomUUIDEntity{
         // Empty Default Constructor
     }
 
-    public Shipment(int weight, boolean pickup, Collection<TrackingPoint> trackingPoints, Collection<TrackingNotification> trackingNotifications) {
+    public Shipment(int weight, boolean pickup, Address sender, Address recipient, Collection<TrackingPoint> trackingPoints, Collection<TrackingNotification> trackingNotifications) {
         this.weight = weight;
         this.pickup = pickup;
+        this.sender = sender;
+        this.recipient = recipient;
         this.trackingPoints = trackingPoints;
         this.trackingNotifications = trackingNotifications;
     }
@@ -59,6 +84,20 @@ public class Shipment extends RandomUUIDEntity{
     public void setTrackingNotifications(Collection<TrackingNotification> trackingNotifications) {
         this.trackingNotifications = trackingNotifications;
     }
-    
-    
+
+    public Address getSender() {
+        return sender;
+    }
+
+    public void setSender(Address sender) {
+        this.sender = sender;
+    }
+
+    public Address getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(Address recipient) {
+        this.recipient = recipient;
+    }
 }

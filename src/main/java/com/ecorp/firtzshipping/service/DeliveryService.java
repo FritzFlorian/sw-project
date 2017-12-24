b3fd2ce081d7cccef0583c56adc3f4e44624582b
FritzFlorian <flo.fritz@t-online.de>
2017-12-24 15:54:07 +0100

    Plan route with tracking points on shipment creation.
    For each shipment a ‘route’ is planned in form of some tracking points that have to be fulfilled to deliver the shipment.

--- src/main/java/com/ecorp/firtzshipping/service/DeliveryService.java
+++ src/main/java/com/ecorp/firtzshipping/service/DeliveryService.java
@@ -1,6 +1,11 @@
 package com.ecorp.firtzshipping.service;
 
 import com.ecorp.fritzshipping.entity.Shipment;
+import com.ecorp.fritzshipping.entity.TrackingPoint;
+import com.ecorp.fritzshipping.entity.TrackingType;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -10,6 +15,8 @@ import javax.transaction.Transactional;
 public class DeliveryService implements DeliveryIF{
     @PersistenceContext
     private EntityManager em;
+    
+    private Random rand = new Random(); 
     
     @Override
     @Transactional
@@ -22,8 +29,38 @@ public class DeliveryService implements DeliveryIF{
         em.persist(shipment);
         // Plan the route for the shipment and persist it.
         // Again this is simplified business logic.
+        planRouteForShipment(shipment);
         
         return shipment;
+    }
+    
+    @Transactional
+    private void planRouteForShipment(Shipment shipment) {
+        List<TrackingPoint> trackingPoints = new LinkedList<>();
+        
+        // Start with pickup vs hand in
+        if (shipment.isPickup()) {
+            trackingPoints.add(createTrackingPoint(TrackingType.PICKUP));
+        } else {
+            trackingPoints.add(createTrackingPoint(TrackingType.HAND_IN));
+        }
+        
+        // Add some random intermidiate steps
+        for (int i = 0; i < rand.nextInt(2) + 1; i++) {
+            trackingPoints.add(createTrackingPoint(TrackingType.PACKAGE_CENTER));
+        }
+        
+        // Finally we deliver the shipment
+        trackingPoints.add(createTrackingPoint(TrackingType.DELIVERY));
+        
+        shipment.setTrackingPoints(trackingPoints);
+    }
+    
+    @Transactional
+    private TrackingPoint createTrackingPoint(TrackingType type) {
+        TrackingPoint newPoint = new TrackingPoint(null, type);
+        em.persist(newPoint);
+        return newPoint;
     }
 
     @Override
